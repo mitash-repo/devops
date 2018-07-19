f8b301c4d4d61f13eeb01dfa97dcfa8a5497d831
Mitash Gupta <mitashgupta1@gmail.com>
2018-07-19 04:20:35 +0000

    added statement

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,7 @@
 public class HelloWorld {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+	//Prints hello git app
 	System.out.println( "Hello git app" );
         System.exit( 0 ); //success
     }
