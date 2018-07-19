8003dc1dd33ab9fe59cfcbc1c2e3462a21f8a40b
Mitash Gupta <mitashgupta1@gmail.com>
2018-07-19 05:01:13 +0000

    making the last changes

--- helloworld.java
+++ helloworld.java
@@ -2,8 +2,6 @@
 public class HelloWorld {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
-	//Prints hello git app
-	System.out.println( "Hello git app" );
         System.exit( 0 ); //success
     }
 }
